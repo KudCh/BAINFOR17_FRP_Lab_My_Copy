00cebd32c8d6d322181474fde30602540c99a008
Perrard Alex <perrardalex@yahoo.com>
2021-12-19 00:06:44 +0100

    Added descriptions of what I have in mind

--- src/main/java/CryptoFeature.java
+++ src/main/java/CryptoFeature.java
@@ -1,5 +1,9 @@
 public class CryptoFeature {
-
+    // Display course and trend of these three cryptocurrencies -> perhaps rotate
+    // https://www.cryptingup.com/api/assets/<ID>
+    // DOGE
+    // BTC
+    // ETH
     CryptoFeature() {
 
     }
--- src/main/java/MemeFeature.java
+++ src/main/java/MemeFeature.java
@@ -1,5 +1,14 @@
 public class MemeFeature {
+    // Whenever the displayed cryptocurrency goes down --> negative meme at random.
+    // Whenever the displayed cryptocurrency goes up --> positive meme at random.
 
+    /* + Positive:
+       + Oprah: 28251713
+       + Boat cat: 1367068
+       - Negative:
+       - Bernie: 222403160
+       - grumpy cat: 405658
+    */
     MemeFeature() {
 
     }
