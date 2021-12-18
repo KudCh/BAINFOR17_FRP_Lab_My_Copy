1d0b70dbde514ae3fe00d4a7c9e71924ea1d6d24
Perrard Alex <perrardalex@yahoo.com>
2021-12-19 00:40:15 +0100

    Fetch for crypto

--- src/main/java/CryptoFeature.java
+++ src/main/java/CryptoFeature.java
@@ -1,10 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class CryptoFeature {
     // Display course and trend of these three cryptocurrencies -> perhaps rotate
     // https://www.cryptingup.com/api/assets/<ID>
     // DOGE
     // BTC
     // ETH
+    //
+    // CURL OR URL FOR BONUS POINTS?
     CryptoFeature() {
+//        // URL
+//        try {
+//            URL url = new URL("https://www.cryptingup.com/api/assets/DOGE");
+//            URLConnection con = url.openConnection();
+//            InputStream is = con.getInputStream();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+//            String line = null;
+//            // read each line and write to System.out
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        //CURL
+//        try {
+//            Process process = Runtime.getRuntime().exec("curl -X GET https://www.cryptingup.com/api/assets/DOGE");
+//            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//            String line = null;
+//            // read each line and write to System.out
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//            process.destroy();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
