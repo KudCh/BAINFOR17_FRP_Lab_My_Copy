5ce3c3655a8e8e9512b4503273246996839243f4
Perrard Alex <perrardalex@yahoo.com>
2021-12-19 01:24:29 +0100

    Memes visualized, need to fetch specific url and display in UI.

--- src/main/java/MemeFeature.java
+++ src/main/java/MemeFeature.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URI;
@@ -11,27 +12,31 @@ public class MemeFeature {
     // Whenever the displayed cryptocurrency goes up --> positive meme at random.
     // Get MEMES: https://api.imgflip.com/get_memes/
     /* + Positive:
-       + Oprah: 28251713
-       + Boat cat: 1367068
+       + Oprah: NR79:  28251713
+       + Boat cat: NR76: 1367068
        - Negative:
-       - Bernie: 222403160
-       - grumpy cat: 405658
+       - Bernie: NR9: 222403160
+       - grumpy cat: NR81: 405658
     */
-    static String uriGetMemes = "https://api.imgflip.com/get_memes/";
+    static String uriGetMemes = "https://api.imgflip.com/get_memes";
     MemeFeature() {
         HttpRequest fetchMemes = HttpRequest.newBuilder()
                 .uri(URI.create(String.format(uriGetMemes)))
                 .build();
         HttpClient httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
+                .version(HttpClient.Version.HTTP_1_1)
                 .connectTimeout(Duration.ofSeconds(10))
                 .build();
         httpClient.sendAsync(fetchMemes, HttpResponse.BodyHandlers.ofString())
                 .thenAcceptAsync(resp -> {
                     try {
-//                        JSONObject jsonObject = new JSONObject(resp.body());
-//                        System.out.println(jsonObject);
-                        System.out.println(resp);
+                        JSONObject allMemes = new JSONObject(resp.body());
+                        JSONObject data = allMemes.getJSONObject("data");
+                        JSONArray memes = data.getJSONArray("memes");
+                        System.out.println(memes);
+                        for (var i=0;i<memes.length();i++) {
+                            System.out.println(i+"    "+memes.get(i));
+                        }
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
