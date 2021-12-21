8a79ae61a91a6f4e901832f3703ddcbc3db227e2
Perrard Alex <perrardalex@yahoo.com>
2021-12-21 20:08:50 +0100

    fix weather

--- src/main/java/WeatherFeature.java
+++ src/main/java/WeatherFeature.java
@@ -31,32 +31,32 @@ public class WeatherFeature {
 
         HttpRequest myReq = HttpRequest.newBuilder().uri(URI.create(String.format(URIGetWeather))).build();
 
-//        //CompletableFuture<HttpResponse<String>>
-//        CompletableFuture<Weather> response = httpClient.sendAsync(myReq, HttpResponse.BodyHandlers.ofString())
-//                .thenApplyAsync(resp ->{
-//                    myObj = new JSONObject(resp.body());
-//                    try {
-//                        JSONArray arr = myObj.getJSONArray("weather");
-//                        JSONObject data1 = arr.getJSONObject(0);
-//                        String weatherD = data1.getString("description");
-//
-//                        JSONObject data2 = myObj.getJSONObject("main");
-//                        Integer temperature = data2.getInt("temp");
-//
-//                        Weather weatherObjj = new Weather(weatherD,temperature);
-//
-//                        return CompletableFuture.completedFuture(weatherObjj);
-//
-//                    } catch (JSONException e) { e.printStackTrace(); }
-//                    return null;
-//                })
-//                .join();
-        return null;
+        //CompletableFuture<HttpResponse<String>>
+        CompletableFuture<Weather> response = httpClient.sendAsync(myReq, HttpResponse.BodyHandlers.ofString())
+                .thenApplyAsync(resp ->{
+                    try {
+                        myObj = new JSONObject(resp.body());
+                        JSONArray arr = myObj.getJSONArray("weather");
+                        JSONObject data1 = arr.getJSONObject(0);
+                        String weatherD = data1.getString("description");
+
+                        JSONObject data2 = myObj.getJSONObject("main");
+                        Integer temperature = data2.getInt("temp");
+
+                        Weather weatherObjj = new Weather(weatherD,temperature);
+
+                        return CompletableFuture.completedFuture(weatherObjj);
+
+                    } catch (JSONException e) { e.printStackTrace(); }
+                    return null;
+                })
+                .join();
+        return response;
     }
 
 
     public WeatherFeature(){
-        final String[] cities = {"Luxembourg", "New York"};
+        final String[] cities = {"Luxembourg", "Berlin","New+York"};
         Observable<String> cityObservable = Observable
                 .interval(2, TimeUnit.SECONDS)
                 .map(Long::intValue)
@@ -71,12 +71,10 @@ public class WeatherFeature {
         currentWeather
                 .observeOn(JavaFxScheduler.platform())
                 .subscribe(wObj -> {
-                    try {
-                        String descr = wObj.weather;
-                        String temp = String.valueOf(wObj.temp);
+                    String descr = wObj.weather;
+                    String temp = String.valueOf(wObj.temp);
 
-                        weatherObjLabel.setText(descr + ", " + temp);
-                    } catch (Exception e){}
+                    weatherObjLabel.setText(descr + ", " + temp);
                 });
 
     }
